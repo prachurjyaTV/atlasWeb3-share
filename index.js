007b467e33a1be6b40a0752703815b29e21c86d0
prachurjyaTV <110392333+prachurjyaTV@users.noreply.github.com>
2022-10-30 12:04:32 +0530

    text align center

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const qrDiv = document.getElementById("qrcode");
-const groupId = new URLSearchParams(window.location.search);
-let id = "";
+setTimeout(() => {
+    const qrDiv = document.getElementById("qrcode");
+    const groupId = new URLSearchParams(window.location.search);
+    let id = "";
 
-for (const param of groupId) {
-    id = param;
-}
+    for (const param of groupId) {
+        id = param;
+    }
 
-const code = new QRCode(qrDiv, {
-    text: id[1],
-    width: 200,
-    height: 200,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-});
+    const code = new QRCode(qrDiv, {
+        text: id[1],
+        width: 200,
+        height: 200,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+    });
+}, 700)
+
